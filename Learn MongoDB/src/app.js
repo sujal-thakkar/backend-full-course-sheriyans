41ecff528b26eda27dad015293b7e6d2f9c9369c
Sujal Thakkar <sujalthakkar153@gmail.com>
2026-04-23 09:57:44 +0530

    Refactor code formatting for consistency in app.js

--- Learn MongoDB/src/app.js
+++ Learn MongoDB/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 
 // POST - create a note and store in DB
-app.post('/notes', async (req, res)=>{
+app.post('/notes', async (req, res) => {
     const data = req.body
     await noteModel.create({
         title: data.title,
@@ -23,14 +23,14 @@ app.get('/notes', async (req, res) => {
     const notes = await noteModel.find() // noteModel.find() returns and array []
     res.status(200).json({
         message: 'notes fetched from DB successfully',
-        notes : notes,
+        notes: notes,
     })
 })
 
 // DELETE - Delete a note from DB
-app.delete('/notes/:id', async (req, res)=>{
+app.delete('/notes/:id', async (req, res) => {
     const id = req.params.id
-    
+
     await noteModel.findOneAndDelete({
         _id: id
     })
@@ -41,11 +41,11 @@ app.delete('/notes/:id', async (req, res)=>{
 })
 
 // PATCH - Update a note from DB
-app.patch('/notes/:id', async(req, res)=>{
+app.patch('/notes/:id', async (req, res) => {
     const id = req.params.id
     const description = req.body.description
 
-    await noteModel.findOneAndUpdate({_id: id}, {description: description})
+    await noteModel.findOneAndUpdate({ _id: id }, { description: description })
 
     res.status(200).json({
         message: 'Updated note successfully'
